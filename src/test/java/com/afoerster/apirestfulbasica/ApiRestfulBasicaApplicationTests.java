ed6352a83c85a500523456fac3f933e0fea53181
Allan <allanf.costa@hotmail.com>
2023-10-23 15:10:02 +0200

    Primeiro commit - Projeto inicial

---
+++ src/test/java/com/afoerster/apirestfulbasica/ApiRestfulBasicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afoerster.apirestfulbasica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulBasicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
